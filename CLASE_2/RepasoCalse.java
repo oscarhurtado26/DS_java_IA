4abba923b2b73064b1c2e00f28eca0f3570fa623
oscarhurtado26 <hurtadooscar198@gmail.com>
2026-06-13 15:16:53 -0500

    Archivo de repaso clase 1

---
+++ CLASE_2/RepasoCalse.java
@@ -0,0 +1,10 @@
+public class RepasoCalse {
+    public static void main(String[] args) {
+        String nombre = "Oscar";
+        Double estatura = 1.84;
+        int edad = 18;
+
+        System.out.println("La edad es: \nnombre");
+    }
+    
+}
